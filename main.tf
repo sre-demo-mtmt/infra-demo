60966a9b4151ab31c1e7e749e3f8f33bc584c398
Matsumoto Eisuke (松本 英介) <matsumoto.eisuke@jp.panasonic.com>
2025-05-28 14:01:28 +0900

    add test comment for atlantis plan

--- main.tf
+++ main.tf
@@ -3,21 +3,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "demo_lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
 resource "aws_lambda_function" "demo" {
   function_name = "demo_lambda"
   handler       = "index.handler"
